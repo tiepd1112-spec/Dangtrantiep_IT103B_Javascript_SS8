9ba30d8d51ac5280bbcdb5927bad729043ec8653
tiepd1112-spec <tiepd1112@gmail.com>
2026-02-24 23:59:19 +0700

    Update b4.js

---
+++ b4.js
@@ -0,0 +1,17 @@
+const players = [
+    "Messi - Forward - 25",
+    "Ronaldo - Forward - 30",
+    "Neymar - Forward - 18",
+    "De Bruyne - Midfielder"
+];
+
+const getReversedNames = (players) => {
+    let names = players.map((player) => {
+        let parts = player.split("-");
+        let name = parts[0];
+        return name;
+    });
+
+    names.reverse();
+    return names;
+};
